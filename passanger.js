fbbcde65e528f94e6f7868cdbfd4a640fb93a7b2
Rogerio <rogeriomanoelrm@gmail.com>
2022-05-16 17:33:09 -0300

    new counter

---
+++ passanger.js
@@ -0,0 +1,27 @@
+
+let savEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+
+    count += 1
+    countEl.textContent = count
+
+}
+
+function save()   {
+
+    let countStr = count + " - "
+    saveEl.textContenttStr
+    countEl.textContent = 0
+    count = 0
+
+
+}
+
+
+    console.log(count)
+
+    
+
